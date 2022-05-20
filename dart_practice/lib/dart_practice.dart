9349e685583ad49534ad791a1d72dd9e7935b27d
junghun <yunjunghun0116@gmail.com>
2022-05-21 03:14:57 +0900

    start about spring and getMapping

---
+++ dart_practice/lib/dart_practice.dart
@@ -0,0 +1,7 @@
+int calculate() {
+  return 6 * 7;
+}
+
+int ohmg(){
+  return 13;
+}
---
+++ javaTest/src/test_1.java
@@ -0,0 +1,7 @@
+public class test_1 {
+    public static void main(String[] args) {
+        System.out.println(1234);
+         byte[] bytes=null;
+        System.out.println(bytes);
+    }
+}
