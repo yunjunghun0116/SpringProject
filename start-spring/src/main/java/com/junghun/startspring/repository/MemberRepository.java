623b9e4deed78a6bda412d3f6c5b4808f3376fca
junghun <yunjunghun0116@gmail.com>
2022-05-22 03:40:38 +0900

    회원 도메인 & 리포지토리 생성

---
+++ start-spring/src/main/java/com/junghun/startspring/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package com.junghun.startspring.repository;
+
+import com.junghun.startspring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}
